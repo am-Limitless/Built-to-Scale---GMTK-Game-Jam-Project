aa40bd756e61618526577b781d7f9ba68baef90c
Gokul Surendran <105282583+am-Limitless@users.noreply.github.com>
2024-08-19 20:13:43 +0530

    death system added

---
+++ Assets/Script/Player/PlayerDeath.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    // This script is for player death
+
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject player;
+
+    private Collider playerCollider;
+    private Collider leftCollider;
+    private Collider rightCollider;
+
+    void Start()
+    {
+        // Get the colliders of the walls & player
+        playerCollider = player.GetComponent<Collider>();
+        leftCollider = leftWall.GetComponent<Collider>();
+        rightCollider = rightWall.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check is the player is pressed
+        if (IsPlayerPressed())
+        {
+            KillPlayer();
+        }
+    }
+
+    bool IsPlayerPressed()
+    {
+        bool isLeftWallTouching = playerCollider.bounds.Intersects(leftCollider.bounds);
+        bool isRightWallTouching = playerCollider.bounds.Intersects(rightCollider.bounds);
+
+        return isLeftWallTouching && isRightWallTouching;
+    }
+
+    void KillPlayer()
+    {
+        Debug.Log("Player is dead");
+
+        Destroy(player);
+    }
+}
